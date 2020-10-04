08c8d175a9080be41d47870cfc8a313ff80084b4
alontzukerman <alontzukerman@gmail.com>
2020-10-04 14:47:38 +0300

    mySolution

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-class Declare{
-  //implement here
+class Declare {
+
+  constructor(executor) {
+      this.successfulQueue = [];
+      this.state = 'pending';
+      this.value;
+
+      executor(this.resolve.bind(this));
+  }
+  runSuccessfulHandlers() {
+      if(this.successfulQueue.length > 0) {
+          const resolution = this.successfulQueue.shift();
+          let returnValue;
+
+          if (typeof resolution.handler === 'function') {
+              returnValue = resolution.handler(this.value);
+          }
+
+          if (returnValue instanceof Declare) {
+              returnValue.then(value => {
+                  resolution.promise.resolve(value);
+              })
+          } else {
+              resolution.promise.resolve(returnValue);
+          }
+      }
+  }
+  resolve(value) {
+      if (this.state === 'pending') {
+          this.value = value;
+          this.state = 'resolved';
+
+          this.runSuccessfulHandlers();
+      }
+  }
+
+  then(successfulHandler) {
+      const newPromise = new Declare(() => {});
+
+      this.successfulQueue.push({
+          handler: successfulHandler,
+          promise: newPromise
+      });
+      if (this.state === 'resolved') {
+          this.runSuccessfulHandlers();
+      }
+      return newPromise;
+  }
 }
-module.exports = Declare
+
+module.exports = Declare;
